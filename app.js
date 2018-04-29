1c4fb0419025b60a0905cc920ce3eeb909fdf43d
gwensw <gwen.simswilliams@gmail.com>
2018-04-29 21:31:49 +0100

    Make training screen fullscreen on mobile

--- app.js
+++ app.js
@@ -277,6 +277,16 @@
         }
       });
     }
+		
+		//make fullscreen if on mobile (header click exists fullscreen)
+		if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
+			if (screenfull.enabled) {
+				screenfull.request();
+				document.getElementById('header').addEventListener('click', () => {
+					screenfull.exit();
+				});
+			}
+		}
   }
   
   // set up and initiate rendering of edit interface for new deck
