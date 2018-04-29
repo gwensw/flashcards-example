405b321e87e41edef7489b6be1758ff745bfa5a3
gwensw <gwen.simswilliams@gmail.com>
2018-04-29 21:40:45 +0100

    Alert for screenfull test 2

--- app.js
+++ app.js
@@ -281,8 +281,8 @@
 		//make fullscreen if on mobile (header click exists fullscreen)
 		if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
 			if (screenfull.enabled) {
-				alert('screenfull');
 				screenfull.request();
+				alert(screenfull.isFullscreen);
 				document.getElementById('header').addEventListener('click', () => {
 					screenfull.exit();
 				});
