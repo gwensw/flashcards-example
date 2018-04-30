a72813e3cf6e6caf7f339174812fba7c6f7c7f3c
gwensw <gwen.simswilliams@gmail.com>
2018-04-30 07:17:38 +0100

    Page now fullscreens as soon as deck is selected

--- app.js
+++ app.js
@@ -221,6 +221,19 @@
     };
     document.querySelector(".main").innerHTML = selectTemplate(context);
     Render.header(false, "Flashcards.js demo");
+		
+		document.querySelectorAll('.deckmenu__deck').forEach( (el) => {
+			el.addEventListener('click', (e) => {
+				//make fullscreen if on mobile (header click exists fullscreen)
+				if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent) && screenfull.enabled) {
+					screenfull.request();
+					alert(screenfull.isFullscreen);
+					document.getElementById('header').addEventListener('click', () => {
+						screenfull.exit();
+					});
+				}
+			}, true);
+		});
   }
   
   // set up and initiate rendering of training interface
@@ -275,25 +288,6 @@
         }
       });
     }
-		document.getElementById('inputAnswer').addEventListener('focus', (e) => {
-			//TODO - make fullscreen if on mobile (header click exists fullscreen)
-			if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
-				const doc = window.document,
-							docEl = doc.documentElement,
-							requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
-
-				if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-					requestFullScreen.call(docEl);
-				}
-	//			if (screenfull.enabled) {
-	//				screenfull.request();
-	//				alert(screenfull.isFullscreen);
-	//				document.getElementById('header').addEventListener('click', () => {
-	//					screenfull.exit();
-	//				});
-	//			}
-			}
-		}, true);
 		
 		drawNextCard();
   }
